3f9eb2484eaaefe2f70eedfe2f8dad6f40f76cb4
gabriel carvajal pizarro <g.carvajal.pizarro@gmail.com>
2020-07-22 21:28:12 -0400

    .

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(webcrapp.ddns.net)
+domains=(services.sbis.cl)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
